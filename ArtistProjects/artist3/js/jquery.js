3bd879c21a0852a858814eac8c5304c83ddddc33
DaveMuller <Dmuller116@gmail.com>
2015-12-22 14:00:02 -0500

    Added other pages, header and footer changes
    Added other pages, header and footer changes

--- ArtistProjects/artist3/js/jquery.js
+++
@@ -1,1 +0,0 @@
-$('.heading, .section1, .section2, .section3').addClass('headline');$('.heading').hide().fadeIn(500);$('.section1').hide().fadeIn(1500);$('.section2').hide().fadeIn(2000);$('.section3').hide().fadeIn(2500);
